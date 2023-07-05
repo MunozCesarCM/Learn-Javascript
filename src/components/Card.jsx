f496f7131b2001160983c679b8f2d8c516657107
MunozCesarCM <munozcesar.cm@gmail.com>
2023-07-05 09:38:21 -0600

    Created complete icon

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,9 @@
-import { TbSeeding, TbCode } from 'react-icons/tb';
+import { TbSeeding, TbCode, TbCircleCheckFilled } from 'react-icons/tb';
 
-const Card = ({ title = 'No title', type = 'Lesson'}) => {
+const Card = ({ title = 'No title', type = 'Lesson', complete = false}) => {
   return (
     <>
-      <div className={`my-4 border-2 ${type === 'Lesson' ? 'border-terminal-green' : 'border-terminal-yellow'} cursor-pointer border-opacity-10 hover:border-opacity-100 duration-100 group`}>
+      <div className={`relative my-4 border-2 ${type === 'Lesson' ? 'border-terminal-green' : 'border-terminal-yellow'} cursor-pointer border-opacity-10 hover:border-opacity-100 duration-100 group`}>
         <div className="px-7 py-6 flex items-top justify-start space-x-6">
           {type === 'Lesson' ? (
             <TbSeeding className="w-8 h-8 text-terminal-green" />
@@ -15,6 +15,9 @@ const Card = ({ title = 'No title', type = 'Lesson'}) => {
             <span className={`text-gray-400 ${type === 'Lesson' ? 'group-hover:text-terminal-green' : 'group-hover:text-terminal-yellow'} duration-50`}>Start {type} →</span>
           </div>
         </div>
+        {complete ? (
+          <TbCircleCheckFilled className={`absolute top-2 right-2 opacity-90 ${type === 'Lesson' ? 'text-terminal-green' : 'text-terminal-yellow'}`} />
+        ) : null}
       </div>
     </>
   );
