949d0d156836f26a2cf22d6c56dec51779778dbc
MunozCesarCM <munozcesar.cm@gmail.com>
2023-07-06 13:28:44 -0600

    Created basic content generation system

--- src/pages/Lesson.jsx
+++ src/pages/Lesson.jsx
@@ -1,11 +1,32 @@
 import Breadcrumb from '../components/Breadcrumb';
 
+const lessonContent = {
+  'what-is-javascript': [
+    {
+      type: 'p',
+      content: 'This is p 1',
+    },
+    {
+      type: 'p',
+      content: 'This is p 2',
+    },
+  ],
+};
+
+const handleTagContent = (tag) => {
+  if (tag.type === 'p') return <p>{tag.content}</p>;
+  return <p className='bg-red-500'>No tag detected</p>;
+}
+
 const SectionOverview = ({ lesson }) => {
   return (
-    <main className='topic-overview'>
+    <main className='lesson'>
       <article>
         <Breadcrumb />
         <h1>{lesson.label}</h1>
+        {lessonContent[lesson.id].map((tag) =>
+          handleTagContent(tag)
+        )}
       </article>
     </main>
   );
