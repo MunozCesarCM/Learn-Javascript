893e77245b84bec3da8d9d564a07710cf14c4b75
MunozCesarCM <munozcesar.cm@gmail.com>
2023-07-07 10:50:27 -0600

    Created code run button

--- src/helpers/handleContentTag.jsx
+++ src/helpers/handleContentTag.jsx
@@ -1,6 +1,7 @@
 import CodeMirror from '@uiw/react-codemirror';
 import { javascript } from '@codemirror/lang-javascript';
-import { tokyoNightStorm } from '@uiw/codemirror-theme-tokyo-night-storm';
+import { tokyoNight } from '@uiw/codemirror-theme-tokyo-night';
+import { TbPlayerPlayFilled } from 'react-icons/tb';
 import { formatContent } from './formatContent';
 
 export const handleTagContent = (tag) => {
@@ -14,19 +15,25 @@ export const handleTagContent = (tag) => {
     return <CodeMirror
       editable={false}
       value={tag.content}
-      theme={tokyoNightStorm}
+      theme={tokyoNight}
       extensions={[javascript()]}
     />
   }
   if (tag.type === 'code') {
     return (
-      <>
+      <div className='relative'>
         <CodeMirror
+          className='mb-6'
           value={tag.content}
-          theme={tokyoNightStorm}
+          theme={tokyoNight}
           extensions={[javascript()]}
         />
-      </>
+        <TbPlayerPlayFilled size={40} className='bg-terminal-yellow rounded-full p-1.5 absolute z-50 -right-2 -translate-y-12 cursor-pointer' />
+        <div className='relative bg-terminal-black text-terminal-white p-4'>
+          <span className='text-slate-500'>{'>'}</span> Run the code above to see your result
+          <span className='fixed text-slate-500 text-xs right-2 bottom-1 select-none'>Console</span>
+        </div>
+      </div>
     );
   }
   return <p className='bg-red-500'>No tag detected</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,14 @@ export default {
     extend: {
       colors: {
         terminal: {
-          black: '#0F111A',
+          black: '#1A1B26',
           grey: '#313855',
           red: '#FF5370',
           yellow: '#FFCB6B',
           green: '#C3E88D',
           blue: '#89DDFF',
           purple: '#C792EA',
+          white: '#C0CAF5',
         }
       }
     },
